2184c32e30378636ee7f89f7c245e0226a777cde
Martin Guillon <martin.guillon@akylas.fr>
2014-10-07 16:13:11 +0200

    a lot

--- Classes/TiTestflightModule.h
+++ Classes/TiTestflightModule.h
@@ -10,4 +10,15 @@
 {
 }
 
+@property(nonatomic,readonly) NSString *OPTION_DISABLE_INAPP_UPDATES; // Defaults to @NO. Setting to @YES, disables the in app update screen shown in BETA apps when there is a new version available on TestFlight.
+@property(nonatomic,readonly) NSString *OPTION_FLUSH_SECOND_INTERVAL; // Defaults to @60. Set to a number. @0 turns off the flush timer. 30 seconds is the minimum flush interval.
+@property(nonatomic,readonly) NSString *OPTION_LONG_ON_CHECKPOINT; // Defaults to @YES. Because logging is synchronous, if you have a high preformance app, you might want to turn this off.
+@property(nonatomic,readonly) NSString *OPTION_LOG_TO_CONSOLE; // Defaults to @YES. Prints remote logs to Apple System Log.
+@property(nonatomic,readonly) NSString *OPTION_LOG_TO_SDTERR; // Defaults to @YES. Sends remote logs to STDERR when debugger is attached.
+@property(nonatomic,readonly) NSString *OPTION_REINSTALL_CRASH_HANDLERS; // If set to @YES: Reinstalls crash handlers, to be used if a third party library installs crash handlers overtop of the TestFlight Crash Handlers.
+@property(nonatomic,readonly) NSString *OPTION_REPORT_CRASHES; // Defaults to @YES. If set to @NO, crash handlers are never installed. Must be set **before** calling `takeOff:`.
+@property(nonatomic,readonly) NSString *OPTION_SEND_ONLY_ON_CRASH; // Defaults to @NO. Setting to @YES stops remote logs from being sent when sessions end. They would only be sent in the event of a crash.
+@property(nonatomic,readonly) NSString *OPTION_SESSION_KEEP_ALIVE_TIMEOUT; // Defaults to @30. This is the amount of time a user can leave the app for and still continue the same session when they come back. If they are away from the app for longer, a new session is created when they come back. Must be a number. Change to @0 to turn off.
+@property(nonatomic,readonly) NSString *OPTION_ASYNC_LOG;
+
 @end
--- Classes/TiTestflightModule.m
+++ Classes/TiTestflightModule.m
@@ -9,9 +9,66 @@
 #import "TiHost.h"
 #import "TiUtils.h"
 #import "TestFlight.h"
+#import "TestFlight+AsyncLogging.h"
 #import "TiApp.h"
 
+
+#define TF_PREFIX @"testflight."
+extern NSString * const TI_APPLICATION_DEPLOYTYPE;
+static BOOL asyncLog = false;
+static BOOL testFilghtOn = false;
+@implementation TiApp (TFLog)
+
++(void)TiNSLog:(NSString*) message
+{
+#pragma push
+#undef NSLog
+    if (testFilghtOn == false || [TI_APPLICATION_DEPLOYTYPE isEqualToString:@"development"]) {
+        NSLog(@"%@",message);
+    }
+    else {
+        if (asyncLog) {
+            TFLog_async(@"[TFA] %@",message);
+        }
+        else {
+            TFLog(@"[TF] %@",message);
+        }
+    }
+#pragma pop
+}
+@end
+
 @implementation TiTestflightModule
+
+static NSDictionary* mapping = nil;
++(NSDictionary*)mapping
+{
+    if (mapping == nil) {
+        mapping = @{
+                    @"disableInAppUpdates":TFOptionDisableInAppUpdates,
+                    @"flushSecondsInterval":TFOptionFlushSecondsInterval,
+                    @"logOnCheckpoint":TFOptionLogOnCheckpoint,
+                    @"lLogToConsole":TFOptionLogToConsole,
+                    @"logToSTDERR":TFOptionLogToSTDERR,
+                    @"reinstallCrashHandlers":TFOptionReinstallCrashHandlers,
+                    @"reportCrashes":TFOptionReportCrashes,
+                    @"sendLogOnlyOnCrash":TFOptionSendLogOnlyOnCrash,
+                    @"sessionKeepAliveTimeout":TFOptionSessionKeepAliveTimeout
+        };
+    }
+    return mapping;
+}
+
+MAKE_SYSTEM_PROP(OPTION_DISABLE_INAPP_UPDATES,@"disableInAppUpdates"); // Defaults to @NO. Setting to @YES, disables the in app update screen shown in BETA apps when there is a new version available on TestFlight.
+MAKE_SYSTEM_PROP(OPTION_FLUSH_SECOND_INTERVAL,@"flushSecondsInterval"); // Defaults to @60. Set to a number. @0 turns off the flush timer. 30 seconds is the minimum flush interval.
+MAKE_SYSTEM_PROP(OPTION_LONG_ON_CHECKPOINT,@"logOnCheckpoint"); // Defaults to @YES. Because logging is synchronous, if you have a high preformance app, you might want to turn this off.
+MAKE_SYSTEM_PROP(OPTION_LOG_TO_CONSOLE,@"lLogToConsole"); // Defaults to @YES. Prints remote logs to Apple System Log.
+MAKE_SYSTEM_PROP(OPTION_LOG_TO_SDTERR,@"logToSTDERR"); // Defaults to @YES. Sends remote logs to STDERR when debugger is attached.
+MAKE_SYSTEM_PROP(OPTION_REINSTALL_CRASH_HANDLERS,@"reinstallCrashHandlers"); // If set to @YES: Reinstalls crash handlers, to be used if a third party library installs crash handlers overtop of the TestFlight Crash Handlers.
+MAKE_SYSTEM_PROP(OPTION_REPORT_CRASHES,@"reportCrashes"); // Defaults to @YES. If set to @NO, crash handlers are never installed. Must be set **before** calling `takeOff:`.
+MAKE_SYSTEM_PROP(OPTION_SEND_ONLY_ON_CRASH,@"sendLogOnlyOnCrash"); // Defaults to @NO. Setting to @YES stops remote logs from being sent when sessions end. They would only be sent in the event of a crash.
+MAKE_SYSTEM_PROP(OPTION_SESSION_KEEP_ALIVE_TIMEOUT,@"sessionKeepAliveTimeout"); // Defaults to @30. This is the amount of time a user can leave the app for and still continue the same session when they come back. If they are away from the app for longer, a new session is created when they come back. Must be a number. Change to @0 to turn off.
+MAKE_SYSTEM_PROP(OPTION_ASYNC_LOG,@"asyncLog"); // Defaults to @30. This is the amount of time a user can leave the app for and still continue the same session when they come back. If they are away from the app for longer, a new session is created when they come back. Must be a number. Change to @0 to turn off.
 
 #pragma mark Internal
 
@@ -34,17 +91,28 @@
 	// this method is called when the module is first loaded
 	// you *must* call the superclass
 	[super startup];
+    NSMutableDictionary* optionsToSet = [NSMutableDictionary dictionary];
     NSDictionary* tiappProperties = [TiApp tiAppProperties];
-    if ([tiappProperties objectForKey:@"testflight.sendLogOnlyOnCrash"]) {
-        [TestFlight setOptions:[NSDictionary dictionaryWithObject:[tiappProperties objectForKey:@"testflight.sendLogOnlyOnCrash"] forKey:@"sendLogOnlyOnCrash"]];
-    }
+    [[TiTestflightModule mapping] enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        id value = [tiappProperties objectForKey:[NSString stringWithFormat:@"%@%@", TF_PREFIX, key]];
+        if (value) {
+            [optionsToSet setObject:value forKey:obj];
+        }
+    }];
+    [TestFlight setOptions:optionsToSet];
+    testFilghtOn = YES;
+    [self replaceValue:@(testFilghtOn) forKey:@"enabled" notification:NO];
     NSString* token = [tiappProperties objectForKey:@"testflight.token"];
+    if ([tiappProperties objectForKey:@"testflight.asyncLog"]) {
+        DebugLog(@"[INFO] TestFlight asyncLogging");
+        asyncLog = [[tiappProperties objectForKey:@"testflight.asyncLog"] boolValue];
+    }
+    [self replaceValue:@(asyncLog) forKey:@"asyncLog" notification:NO];
     
     if (token) {
         DebugLog(@"[INFO] TestFlight takeOff %@", token);
         [TestFlight takeOff:token];
     }
-	DebugLog(@"[INFO] %@ loaded",self);
 }
 
 -(void)shutdown:(id)sender
@@ -105,8 +173,8 @@
     ENSURE_UI_THREAD(takeOff, args);
     ENSURE_SINGLE_ARG_OR_NIL(args,NSString);
     
-    DebugLog(@"[INFO] TestFlight takeOff %@", args);
     [TestFlight takeOff:args];
+    DebugLog(@"[INFO] TestFlight takeOff %@", args);
 }
 
 -(void)passCheckpoint:(id)args
@@ -143,9 +211,7 @@
     ENSURE_ARG_OR_NIL_AT_INDEX(value, args, 1, NSString);
 
     if (key) {
-        //we have one
         if (value) {
-            //we have 2
             TFLog(@"[%@] %@",key,value);
         }
         else {
@@ -154,20 +220,38 @@
     }
 }
 
+
+
 -(id)sdkVersion:(id)args
 {
     return TESTFLIGHT_SDK_VERSION;
 }
 
-/*- (id)setOptions:(id)args
+- (void)setOptions:(id)args
 {
     ENSURE_UI_THREAD(setOptions, args);
-    
-    ENSURE_ARG_COUNT(args, 1);
-    id options = [args objectAtIndex:0];
-    ENSURE_DICT(options);
-    [TestFlight setOptions:options];
-    return nil;
-}*/
+    ENSURE_SINGLE_ARG(args, NSDictionary);
+    NSMutableDictionary* optionsToSet = [NSMutableDictionary dictionary];
+    [args enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        id value = [[TiTestflightModule mapping] objectForKey:key];
+        if (value) {
+            [optionsToSet setObject:obj forKey:value];
+        }
+    }];
+    [TestFlight setOptions:optionsToSet];
+}
+
+-(void)setAsyncLog:(id)val
+{
+    asyncLog  = [TiUtils boolValue:val def:asyncLog];
+    [self replaceValue:@(asyncLog) forKey:@"asyncLog" notification:NO];
+}
+
+-(void)setEnabled:(id)val
+{
+    testFilghtOn  = [TiUtils boolValue:val def:testFilghtOn];
+    [self replaceValue:@(testFilghtOn) forKey:@"enabled" notification:NO];
+}
+
 
 @end
